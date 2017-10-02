d9f8f39aeb8effaae2ce63aa102d905ea8b27278
jpthesolver2 <jpbulman@wpi.edu>
2017-10-02 13:30:04 -0400

    Tweaks

---
+++ Java/src/src/Edge.java
@@ -0,0 +1,16 @@
+/**
+ * Created by JP Bulman on 9/25/2017.
+ */
+public class Edge {
+
+    public int esticker1,esticker2;
+
+    public Edge(int sticker1, int sticker2){
+
+        this.esticker1=sticker1;
+        this.esticker2=sticker2;
+
+    }
+
+
+}
